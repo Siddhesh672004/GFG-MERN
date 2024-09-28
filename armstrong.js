28ac2c36c27f9e2caf0c6f1db9ec85bfe17e91ff
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2024-09-29 00:18:58 +0530

    Week 2 (Day 1)

---
+++ armstrong.js
@@ -0,0 +1,15 @@
+var n = 150;
+var temp = n;
+var sum = 0;
+
+while(temp > 0){
+	var digit = temp % 10;
+   sum += digit ** 3;
+   temp = parseInt(temp/10);
+}
+
+if(sum === n){
+	console.log("Armstrong Number");
+}else{
+	console.log("Not an Armstrong Number");
+}
